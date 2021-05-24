6e2df5db2af9822bad4b9472012e7e20c3c8293c
Thejus Paul <thejuspaul@pm.me>
2021-05-24 22:03:32 +0530

    feat: add description page

--- src/components/description.js
+++ src/components/description.js
@@ -1,9 +1,29 @@
 import { useHistory } from "react-router-dom";
+import './description.css';
 
 const Description = () => {
     const history = useHistory();
     return(
-        <p>Description</p>
+        <div className="description">
+            <span className="title">Product Description</span>
+            <span className="subtitle">Paste your product or service description here to let us answer your queries.</span>
+            <form className="description-form">
+                <div className="input-box">
+                    <label htmlFor="name">Product Description</label>
+                    <textarea
+                    className="description"
+                    aria-label="Enter the Product Description:" 
+                    type="text" 
+                    name="name"
+                    onChange={(e) => console.log("Description: "+e.target.value)}
+                    />
+                </div>
+                <div className="buttons">
+                    <button className="submit-btn" type="submit">Submit</button>
+                    <button className="clear-btn" type="reset">Clear</button>
+                </div>
+            </form>
+        </div>
     )
 }
 
