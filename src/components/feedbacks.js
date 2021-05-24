456f09a14ba04ce051b586da595da9796400a770
Thejus Paul <thejuspaul@pm.me>
2021-05-24 22:17:34 +0530

    feat: add feedback page

--- src/components/feedbacks.js
+++ src/components/feedbacks.js
@@ -1,6 +1,16 @@
+import './feedbacks.css';
+
 const Feedbacks = () => {
     return(
-        <p>Feedbacks</p>
+        <div className="feedbacks">
+            <span className="title">Customer Feedbacks</span>
+            <span className="subtitle">This section will lets you know whether your customers are satisfied with your product or service.</span>
+            <div className="buttons">
+                <button className="outline-btn">WhatsApp</button>
+                <button className="outline-btn">Telegram</button>
+                <button className="outline-btn">Website</button>
+            </div>
+        </div>
     )
 }
 
