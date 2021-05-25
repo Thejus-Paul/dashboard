ff97486206ee6a0539334a3e8382799420760ac5
Thejus Paul <thejuspaul@pm.me>
2021-05-25 07:46:16 +0530

    feat: add queries and complaints page

--- src/components/complaints.js
+++ src/components/complaints.js
@@ -1,6 +1,51 @@
+import { useState } from 'react';
+import './complaints.css';
+
 const Complaints = () => {
+    const [complaints, setComplaints] = useState([{
+            name: "Thejus Paul",
+            place: "Maradu",
+            message: "The cookies tasted good but, I had ordered 5 cookies and only 3 cookies were delivered.",
+            phone: 9633009041,
+            api: 'IdWchAkl3C'
+        },{
+            name: "Antony Clinton",
+            place: "Fort Kochi",
+            message: "The cookies you delivered tasted very bad.",
+            phone: 9633009041,
+            api: 'IdWchAkl3C'
+        }
+    ]);
+
     return(
-        <p>Complaints</p>
+        <div className="complaints">
+            <span className="title">Customer Complaints</span>
+            <span className="subtitle">Your customers expression of dissatisfaction can be found here</span>
+            <div className="buttons">
+                <button className="outline-btn">WhatsApp</button>
+                <button className="outline-btn">Telegram</button>
+                <button className="outline-btn">Website</button>
+            </div>
+
+            <div className="complaints-list">            
+                { complaints.map((complaint,index) => {
+                        return(
+                            <div className="complaint" key={index}>
+                                <div className="header">
+                                    <span className="heading">{complaint.name + ', ' + complaint.place}</span>
+                                </div>
+                                <div className="body">
+                                    <span className="message">{complaint.message}</span>
+                                    <div className="actions">
+                                        <button className="outline-btn">Call</button>
+                                        <button className="outline-btn">Reply</button>
+                                    </div>
+                                </div>
+                            </div>
+                        )})
+                }
+            </div>
+        </div>
     )
 }
 
--- src/components/queries.js
+++ src/components/queries.js
@@ -1,6 +1,41 @@
+import { useState } from 'react';
+import './queries.css';
+
 const Queries = () => {
+    const [queries, setQueries] = useState([{
+        message: "What are your offers?",
+        api: 'IdWchAkl3C'
+    },
+    {
+        message: "Can you mention your working hours?",
+        api: 'IdWchAkl3C'
+    }
+]);
     return(
-        <p>Queries</p>
+        <div className="queries">
+            <span className="title">Customer Queries</span>
+            <span className="subtitle">The queries that were left unresolved can be found here. You may click <code>Reply</code> to respond to each message.</span>
+            <div className="buttons">
+                <button className="outline-btn">WhatsApp</button>
+                <button className="outline-btn">Telegram</button>
+                <button className="outline-btn">Website</button>
+            </div>
+            <div className="queries-list">            
+                { queries.map((query,index) => {
+                        return(
+                            <div className="query" key={index}>
+                                <div className="body">
+                                    <span className="message">{query.message}</span>
+                                    <div className="actions">
+                                        <button className="submit-btn">Reply</button>
+                                        <button className="clear-btn">Discard</button>
+                                    </div>
+                                </div>
+                            </div>
+                        )})
+                }
+            </div>
+        </div>
     )
 }
 
