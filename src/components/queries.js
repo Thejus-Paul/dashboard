def2d675f16133aa1da406f76470ff21eb7bdb09
Thejus Paul <32426070+Thejus-Paul@users.noreply.github.com>
2021-06-11 15:40:28 +0530

    fix: queries page functionality (#28)
    * fix: fetches from database
    
    * fix: some minor UI issue
    
    * feat: add delete function

--- src/components/queries.js
+++ src/components/queries.js
@@ -1,51 +1,48 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './queries.css';
 
 const Queries = () => {
-    const [queries, setQueries] = useState([{
-        message: "What are your offers?",
-        time: 1622255697778
-    },
-    {
-        message: "Can you mention all discounts you provide?",
-        time: 1522255497778
-    },
-    {
-        message: "Can I order online?",
-        time: 1522255597778
-    },
-    {
-        message: "Can you mention your working hours?",
-        time: 1522255697778
-    },
-    {
-        message: "Do you have cashew cookies?",
-        time: 1422255697778
+    const [queries, setQueries] = useState([]);
+    useEffect(() => {
+        fetch('https://sponge-imminent-text.glitch.me/cookiepoint/queries')
+        .then(response => response.json())
+        .then(response => setQueries((response.queries).reverse()));
+    }, [queries]);
+
+    const deleteItem = (index) => {
+        queries.splice(index, 1);
+        fetch("https://sponge-imminent-text.glitch.me/cookiepoint/queries", {
+            method: 'post',
+            mode: 'cors',
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            },
+            body: JSON.stringify(queries.reverse())
+        }).then(setQueries(queries));
     }
-]);
+
     return(
         <div className="queries">
             <span className="title">Customer Queries</span>
-            <span className="subtitle">The queries that were left unresolved can be found here. You may click <code>Reply</code> to respond to each message.</span>
-            {/* <div className="buttons">
-                <button className="outline-btn">Negotiation</button>
-                <button className="outline-btn">Payment</button>
-                <button className="outline-btn">Delivery</button>
-                <button className="outline-btn">Discounts</button>
-                <button className="outline-btn">Quality - Manufacturing</button>
-                <button className="outline-btn">Post Purchase Support</button>
-
-            </div> */}
+            <span className="subtitle">The queries that were left unresolved can be found here. You can call the customer to resolve their queries. You may also update the abstract for the future customers.</span>
             <div className="queries-list">            
-                { queries.map((query,index) => {
+                { queries.length > 0 ? queries.map((query,index) => {
                         return(
                             <div className="query" key={index}>
                                 <div className="body">
                                     <span className="message">
-                                        <span>{query.message}</span> - <em>{new Date(query.time).toGMTString()}</em></span>
+                                        <span>{query.unanswered_qa_text}</span> - {query.customer_name} <em>{new Date(query.time).toGMTString()}</em></span>
+                                        <div className="actions">
+                                        <a href={`tel:${query.customer_mob_no}`} style={{backgroundColor: 'transparent'}}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#2980b9" viewBox="0 0 24 24"><path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"/></svg>								
+                                        </a>
+                                        <button style={{backgroundColor: 'transparent'}} onClick={() => deleteItem(index)}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="#e63d3e" width="24" height="24" viewBox="0 0 24 24"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/></svg>								
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
-                        )})
+                        )}) : <em>Either the query list is being fetched OR there aren't any queries at the moment.</em>
                 }
             </div>
         </div>
