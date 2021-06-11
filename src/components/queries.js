180f16b25d016fa93ab2a32d6edd1ec546d6e3b8
Thejus Paul <thejuspaul@pm.me>
2021-06-11 15:34:58 +0530

    feat: add delete function

--- src/components/queries.js
+++ src/components/queries.js
@@ -9,6 +9,18 @@ const Queries = () => {
         .then(response => setQueries((response.queries).reverse()));
     }, [queries]);
 
+    const deleteItem = (index) => {
+        queries.splice(index, 1);
+        fetch("https://sponge-imminent-text.glitch.me/cookiepoint/queries", {
+            method: 'post',
+            mode: 'cors',
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            },
+            body: JSON.stringify(queries.reverse())
+        }).then(setQueries(queries));
+    }
+
     return(
         <div className="queries">
             <span className="title">Customer Queries</span>
@@ -24,7 +36,7 @@ const Queries = () => {
                                         <a href={`tel:${query.customer_mob_no}`} style={{backgroundColor: 'transparent'}}>
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#2980b9" viewBox="0 0 24 24"><path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"/></svg>								
                                         </a>
-                                        <button style={{backgroundColor: 'transparent'}} >
+                                        <button style={{backgroundColor: 'transparent'}} onClick={() => deleteItem(index)}>
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="#e63d3e" width="24" height="24" viewBox="0 0 24 24"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/></svg>								
                                         </button>
                                     </div>
