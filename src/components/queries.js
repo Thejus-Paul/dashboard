3db1459bc0c3849faf8a8fee9920618aa558fd27
Thejus Paul <thejuspaul@pm.me>
2021-06-14 23:35:22 +0530

    feat: add live order form

--- src/components/queries.js
+++ src/components/queries.js
@@ -31,7 +31,7 @@ const Queries = () => {
                             <div className="query" key={index}>
                                 <div className="body">
                                     <span className="message">
-                                        <span>{query.unanswered_qa_text}</span> - {query.customer_name} <em>{new Date(query.time).toGMTString()}</em></span>
+                                        <span>{query.unanswered_qa_text}</span> - {query.customer_name} <em>{new Date(query.time).toString()}</em></span>
                                         <div className="actions">
                                         <a href={`tel:${query.customer_mob_no}`} style={{backgroundColor: 'transparent'}}>
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#2980b9" viewBox="0 0 24 24"><path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"/></svg>								
---
+++ src/pages/form.js
@@ -0,0 +1,133 @@
+import './login.css';
+import './form.css';
+import { useHistory } from 'react-router';
+import { useEffect, useState } from 'react';
+
+export default function Form() {
+    const history = useHistory();
+    const [questions, setQuestions] = useState([]);
+    const [isOrderPlaced, setOrderPlaced] = useState(false);
+    const [liveOrders, setLiveOrders] = useState([]);
+    const [newOrder, setNewOrder] = useState([]);
+    const [items, setItems] = useState([]);
+    const [total, setTotal] = useState(0);
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_SUP_PORT_API}/cookiepoint/questions`)
+        .then(response => response.json())
+        .then(response => setQuestions(response.questions));
+
+        fetch(`${process.env.REACT_APP_SUP_PORT_API}/cookiepoint/live_orders`)
+        .then(response => response.json())
+        .then(response => setLiveOrders(response.orders));
+    }, [])
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_SUP_PORT_API}/cookiepoint/catalog`)
+        .then(response => response.json())
+        .then(response => setItems(response.items));
+    }, [items]);
+
+    const handleResponse = (e) => {
+        e.preventDefault();
+        if(isOrderPlaced) {
+            let newOrderList = [...liveOrders];
+            newOrderList.push(newOrder);
+            fetch(`${process.env.REACT_APP_SUP_PORT_API}/cookiepoint/live_orders`, {
+                method: 'post',
+                mode: 'cors',
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                },
+                body: JSON.stringify(newOrderList)
+            }).then(console.log("Done!"));
+        } else {
+            let newOrder = {}
+            for(let item = 0; item < (e.target.length - 1); ++item) {
+                newOrder[e.target[item].name] = isNaN(e.target[item].value) ? e.target[item].value : Number(e.target[item].value);
+            }
+            let total = 0;
+            for(let item of items) {
+                total += (newOrder[item.name]) * item.price;
+            }
+            newOrder["order_no"] = liveOrders.length + 1;
+            newOrder["order_status"] = "Open";
+            console.log(newOrder);
+            setTotal(total);
+            setNewOrder(newOrder);
+            setOrderPlaced(true);
+        }
+        //history.push('/home')
+    }
+    return(
+        <div className="login padding-top-2 scroll">
+            <div className="input-card">
+                <div className="form forms">
+                    <span className="title">Order Form</span><br/>
+                    <form onSubmit={handleResponse}>
+                        {
+                            isOrderPlaced ?  
+                                    <div>
+                                        <br/>
+                                        <h3>Order Summary</h3><br/>
+                                        <span><strong>Name:</strong> {newOrder.name}</span><br/>
+                                        <span><strong>Address:</strong> {newOrder.address}</span><br/>
+                                        <span><strong>Items</strong></span><br/>
+                                        {
+                                            items.map((item,index) => {
+                                                return(
+                                                newOrder[item.name] !== 0 ? 
+                                                <span key={index} className="order-summary">
+                                                    <span>{item.name}  x {newOrder[item.name]}</span>
+                                                    <span>Rs.{newOrder[item.name] * item.price}</span>
+                                                </span> : ''
+                                                )
+                                                })
+                                        }
+                                        <span style={{display:"flex",justifyContent:"flex-end"}}><strong>Total:{total}</strong></span>
+                                    </div>
+                                        :
+                            questions.map((question,index) => {
+                                return(
+                                    <div className="input-box" key={index}>
+                                        <label htmlFor={question.question}>{`Enter your ${question.question}:`}</label>
+                                        <input 
+                                        aria-label={`Enter your ${question.question}:`}
+                                        type={question.type}
+                                        name={question.question}
+                                        />
+                                    </div>
+                                )
+                            })
+                        }
+                        {
+                            !isOrderPlaced ? items.map((item,index) => {
+                                return(
+                                    <div className="input-box" key={index}>
+                                        <label htmlFor={item.name}>
+                                            <img 
+                                            src={item.image} 
+                                            width="200px"
+                                            alt={item.name}
+                                            /><br/>{`Price: Rs.${item.price}`}&nbsp;&nbsp;&nbsp;&nbsp;{`Quantity Left: ${item.quantity}`}<br/><br/>
+                                            {`Enter the quantity of ${item.name}:`}
+                                        </label>
+                                        <input 
+                                        aria-label={`Enter the quantity of ${item.name}:`}
+                                        type="number"
+                                        name={item.name}
+                                        min="0"
+                                        max={item.quantity}
+                                        />
+                                    </div>
+                                )
+                            }): ''
+                        }
+                        <br/>
+                        <button className="login-btn margin-bottom-2" type="submit">Place Order</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
